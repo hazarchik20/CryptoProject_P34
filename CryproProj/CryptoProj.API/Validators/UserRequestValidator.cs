a275be9d80a04bcc485b6626834e783fb6726f72
hazarchik20 <nazariypan2702@gmail.com>
2026-03-01 10:35:12 +0200

    add validator and unit test

---
+++ CryproProj/CryptoProj.API/Validators/UserRequestValidator.cs
@@ -0,0 +1,50 @@
+﻿using CryptoProj.Domain.Services.Users;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace CryptoProj.API.Validators
+{
+    public class UserRequestValidator{
+        public static bool ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            return Regex.IsMatch(name, @"^[a-zA-Z0-9_]+$");
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return false;
+
+
+            if (password.Length <= 8)
+                return false;
+
+            if (!password.Any(char.IsUpper))
+                return false;
+
+            if (!password.Any(char.IsLower))
+                return false;
+
+            if (!password.Any(c => "!;%@*".Contains(c)))
+                return false;
+
+            if (!password.Any(c => c >= '\u2648' && c <= '\u2653'))
+                return false;
+
+            var romanPattern = @"\b(M{0,4}(CM|CD|D?C{0,3})
+                              (XC|XL|L?X{0,3})
+                              (IX|IV|V?I{0,3}))\b";
+
+            if (!Regex.IsMatch(password, romanPattern, RegexOptions.IgnorePatternWhitespace))
+                return false;
+
+            return true;
+        }
+
+
+
+    }
+}
---
+++ CryproProj/TestProject.Tests/UserValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿using CryptoProj.API.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Tests
+{
+    public class UserValidatorTests
+    {
+
+        // -------- ValidateName ------
+
+        [Fact]
+        public void ValidateName_ValidName_ReturnsTrue()
+        {
+            var result = UserRequestValidator.ValidateName("User_123");
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("John")]
+        [InlineData("user_1")]
+        [InlineData("A1B2C3")]
+        [InlineData("Test_User")]
+        public void ValidateName_ValidNames_ReturnsTrue(string name)
+        {
+            var result = UserRequestValidator.ValidateName(name);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("John Doe")]
+        [InlineData("User-123")]
+        [InlineData("User@123")]
+        [InlineData("!User")]
+        [InlineData("")]
+        public void ValidateName_InvalidNames_ReturnsFalse(string name)
+        {
+            var result = UserRequestValidator.ValidateName(name);
+
+            Assert.False(result);
+        }
+
+
+
+
+        // ------ ValidatePassword ---------
+
+        [Fact]
+        public void ValidatePassword_ValidPassword_ReturnsTrue()
+        {
+            var password = "StrongPass@XI♈";
+
+            var result = UserRequestValidator.ValidatePassword(password);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("Aa12345!XI♈")]
+        [InlineData("Password@IV♉")]
+        [InlineData("GoodPass%M♓")]
+        public void ValidatePassword_ValidPasswords_ReturnsTrue(string password)
+        {
+            var result = UserRequestValidator.ValidatePassword(password);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("Short1!I♈")]          
+        [InlineData("nouppercase@XI♈")]    
+        [InlineData("NOLOWERCASE@XI♈")]    
+        [InlineData("NoSpecialXI♈")]     
+        [InlineData("NoZodiac@XI")]      
+        [InlineData("NoRoman@♈")]          
+        public void ValidatePassword_InvalidPasswords_ReturnsFalse(string password)
+        {
+            var result = UserRequestValidator.ValidatePassword(password);
+
+            Assert.False(result);
+        }
+
+
+    }
+}
